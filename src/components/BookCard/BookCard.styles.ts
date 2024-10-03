0e66ea3b15e36d41b96675f1edb3b512927e30af
Remerson Monteiro <kairemerson@gmail.com>
2024-10-02 22:02:02 -0300

    feat: created componet bookcard

---
+++ src/components/BookCard/BookCard.styles.ts
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import styled, { css } from "styled-components";
+
+export const Container = styled(Link)`
+    ${({theme})=> css`
+    
+    `}
+`
+
+export const Thumbnail = styled.img`
+    ${({theme})=> css`
+    
+    `}
+`
+
+export const Details = styled.div`
+    ${({theme})=> css`
+    
+    `}
+`
